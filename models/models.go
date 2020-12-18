725db38a0b580a871f1383c0beddeff82ef804d7
gonzalezlrjesus@gmail.com <gonzalezlrjesus@gmail.com>
2020-12-18 18:38:08 -0400

    fix: change format to fill DB configuration var to a correctly way

--- models/models.go
+++ models/models.go
@@ -20,12 +20,12 @@ func init() {
 		log.Print(e)
 	}
 
-	username := os.Getenv("db_user")
-	password := os.Getenv("db_pass")
-	dbName := os.Getenv("db_name")
-	dbHost := os.Getenv("db_host")
-
-	dbURI := fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", dbHost, username, dbName, password)
+	// DB config reading
+	dbURI := os.Getenv("DATABASE_URL")
+	if dbURI == "" {
+		dbURI = fmt.Sprintf("host=%s user=%s dbname=%s sslmode=disable password=%s", os.Getenv("db_host"), os.Getenv("db_user"), os.Getenv("db_name"), os.Getenv("db_pass"))
+	}
+	log.Println(dbURI)
 
 	conn, err := gorm.Open("postgres", dbURI)
 	if err != nil {
